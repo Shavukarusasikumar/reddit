90a3fa1fc70251e20ee015361583ea4246ee19be
utkarsh <utkarshs9727@gmail.com>
2025-07-18 20:12:29 +0530

    implement post and postvote entities

---
+++ src/main/java/com/mb/reddit/entity/Post.java
@@ -0,0 +1,50 @@
+package com.mb.reddit.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "posts")
+@Data
+@NoArgsConstructor
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "media_url")
+    private String mediaUrl;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @ManyToOne
+    private User author;
+
+    @ManyToOne
+    private Community community;
+
+    @OneToMany(mappedBy = "post")
+    private List<Comment> comments;
+
+    @OneToMany(mappedBy = "post")
+    private List<PostVote> postVotes;
+
+    @ManyToMany(mappedBy = "savedPosts", cascade = CascadeType.ALL)
+    private List<User> savedByUser;
+}
---
+++ src/main/java/com/mb/reddit/entity/PostVote.java
@@ -0,0 +1,35 @@
+package com.mb.reddit.entity;
+
+import jakarta.persistence.*;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "post_votes", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"user_id", "post_id"})})
+@Data
+@NoArgsConstructor
+public class PostVote {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @Column(name = "is_like")
+    private Boolean isLike;
+
+    @Column(name = "liked_at")
+    private LocalDateTime likedAt;
+}
