35a118612e1869958333ae6920e8f0e6e42e129c
Sahil Sapehia <officialsahilsapehia03@gmail.com>
2025-07-19 15:07:55 +0530

    implement delete and update comment method in comment controller

--- src/main/java/com/mb/reddit/service/CommentService.java
+++ src/main/java/com/mb/reddit/service/CommentService.java
@@ -9,7 +9,7 @@ public interface CommentService {
 
 	Comment createComment(String content, Long userId, Long postId, Long parentCommentId);
 	void deleteCommentById(Long id);
-	Comment updateComment(Long commentId, Long userId, String updatedContent);
+	Comment updateComment(Long commentId, String updatedContent);
 	List<CommentVote> getCommentVotesByCommentId(Long commentId);
 	List<Comment> getRepliesByCommentId(Long commentId);
 	Comment getCommentById(Long id);
