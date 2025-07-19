1f8b6e8b15630e8508d280be21c357c3cbbb1401
Sahil Sapehia <officialsahilsapehia03@gmail.com>
2025-07-19 10:45:42 +0530

    implement getPostById method in post controller

--- src/main/java/com/mb/reddit/controller/PostController.java
+++ src/main/java/com/mb/reddit/controller/PostController.java
@@ -1,7 +1,11 @@
 package com.mb.reddit.controller;
 
+import com.mb.reddit.entity.Post;
 import com.mb.reddit.service.PostService;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -14,6 +18,14 @@ public class PostController {
         this.postService = postService;
     }
 
+    @GetMapping("/posts/{postId}")
+    public String getPostById(Long postId, Model model) {
+        Post post = postService.getPostById(postId);
+
+        model.addAttribute("post", post);
+
+        return "view-post";
+    }
     @PostMapping("/delete/post/{postId}")
     public String deletePostById(@PathVariable("postId") Long postId) {
         postService.deletePost(postId);
