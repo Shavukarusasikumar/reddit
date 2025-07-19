019a50d2d7d3f49700867b886ee34302b8b585ee
Deepak <deepakkotamsetti16@gmail.com>
2025-07-19 10:46:12 +0530

    implement getAllPosts controller

--- src/main/java/com/mb/reddit/controller/PostController.java
+++ src/main/java/com/mb/reddit/controller/PostController.java
@@ -3,7 +3,12 @@ package com.mb.reddit.controller;
 import com.mb.reddit.entity.Post;
 import com.mb.reddit.service.PostService;
 
+
+import org.springframework.ui.Model;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +23,17 @@ public class PostController {
         this.postService = postService;
     }
 
+    @GetMapping
+    public String getAllPosts(@RequestParam(defaultValue = "0", required = false) int pageNumber,
+                              @RequestParam(defaultValue = "10", required = false) int pageSize,
+                              @RequestParam(defaultValue = "publishedAt", required = false) String sortBy,
+                              Model model) {
+        Page<Post> posts = postService.getAllPost(pageNumber, pageSize, sortBy);
+
+        model.addAttribute("posts", posts);
+
+        return "home";
+    }
     @GetMapping("/posts/{postId}")
     public String getPostById(Long postId, Model model) {
         Post post = postService.getPostById(postId);
