15492c819037a5656e26fce417249eab50625e25
Sahil Sapehia <officialsahilsapehia03@gmail.com>
2025-07-21 09:57:23 +0530

    update create-community in service class

--- src/main/java/com/mb/reddit/service/implementation/CommunityServiceImpl.java
+++ src/main/java/com/mb/reddit/service/implementation/CommunityServiceImpl.java
@@ -15,6 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -37,7 +38,8 @@ public class CommunityServiceImpl implements CommunityService {
     @Override
     public Community createCommunity(Community community, MultipartFile fileIcon, MultipartFile fileBanner) {
         User user = userService.getCurrentUser();
-
+        community.setCreator(user);
+        community.setCreatedAt(LocalDateTime.now());
         if(fileIcon != null) {
             try {
                 String iconUrl = cloudinaryService.uploadFile(fileIcon);
