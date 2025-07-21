f1e54c909de99bf06b31098c0faec36cc4a3d671
Sahil Sapehia <officialsahilsapehia03@gmail.com>
2025-07-22 00:09:27 +0530

    implement chatRoom functionality for chat-serivce

---
+++ src/main/java/com/mb/reddit/message/chatRoom/ChatRoom.java
@@ -0,0 +1,25 @@
+package com.mb.reddit.message.chatRoom;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatRoom {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String chatId;
+    private String senderId;
+    private String recipientId;
+}
