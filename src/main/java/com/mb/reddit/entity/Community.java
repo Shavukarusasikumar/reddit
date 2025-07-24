ba68621d14e42131d546bbe021d543b67c04b18c
Sasikumar <shavukarusasikumar@gmail.com>
2025-07-24 18:57:40 +0530

    changed database

--- src/main/java/com/mb/reddit/entity/Community.java
+++ src/main/java/com/mb/reddit/entity/Community.java
@@ -21,6 +21,7 @@ public class Community {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true)
     private String name;
 
     @Column(columnDefinition = "TEXT")
